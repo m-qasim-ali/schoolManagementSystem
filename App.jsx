e3f33c835398d8a69201f26a30386dba980060c5
Zohaib-Munir <zohaib.khan123178@gmail.com>
2023-12-24 16:55:15 +0500

    Homewrok

--- App.jsx
+++ App.jsx
@@ -1,30 +1,88 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+// In App.js in a new project
 
-function App() {
+import * as React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+function HomeScreen() {
   return (
-    <View>
-      <Text>Hello world!..</Text>
+    <View style={Styles.parentView}>
+      <View style={Styles.questionDescriptionSection}>
+        <Text style={Styles.questionTittle}>
+          Home Work Description Text Here
+        </Text>
+        <Text>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur
+          ducimus reprehenderit asperiores. Corporis, voluptatibus. Tenetur
+          perferendis quaerat voluptatibus, cupiditate inventore quasi aperiam
+          laudantium dolores quis praesentium tempore itaque omnis reiciendis,
+          ad repudiandae.
+        </Text>
+      </View>
+
+      <View style={Styles.questionMaterialSection}>
+        <Text>Related Description text Here</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="HOMEWORK"
+          component={HomeScreen}
+          options={{
+            headerLeft: () => (
+              <View style={Styles.headerLeftContainer}>
+                <Image source={require('./assets/images/homeIcon.png')}></Image>
+              </View>
+            ),
+            headerStyle: {
+              backgroundColor: '#0C46C4',
+            },
+
+            headerTitleStyle: {
+              fontSize: 20,
+              fontWeight: 'bold',
+              color: 'white',
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const Styles = StyleSheet.create({
+  parentView: {
+    flex: 1,
+    width: '85%',
+    alignSelf: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  headerLeftContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  questionDescriptionSection: {
+    flex: 1,
+    justifyContent: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  questionMaterialSection: {
+    flex: 2,
+  },
+  questionTittle: {
+    fontWeight: '900',
+    fontSize: 15,
+    color: 'black',
   },
 });
 
